50b02a2e032d788b6f834f4cf1e2877a07074584
Komal Pathare <117629456+komalpathare8755@users.noreply.github.com>
2023-03-19 22:53:09 +0530

    Add files via upload

---
+++ Program1_2.ts
@@ -0,0 +1,15 @@
+
+function Area(Rad:number,PI:number=3.14):number
+{
+    let Ans:number=0;
+
+    Ans=PI*Rad*Rad;
+
+    return Ans ;
+}
+
+var Ret : number=0;
+
+ Ret=Area(5);
+
+console.log("Area of  circle is "+Ret);
